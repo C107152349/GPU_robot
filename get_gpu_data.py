99501e843a177f62634d6b546262365dd55c4a79
C107152349 <C107152349@nkust.edu.tw>
2022-05-06 03:52:52 +0800

    update get_gpu_data.py

--- get_gpu_data.py
+++ get_gpu_data.py
@@ -1,45 +1,70 @@
-from selenium import webdriver
-from selenium.webdriver.edge.options import Options
-from selenium.webdriver.common.by import By
-import time
-class GPU:
-    def __init__(self,name,pn,price):
-        self.name = name
-        self.pn = pn
-        self.price = price
-op = Options()
-op.add_argument("--disable-notifications")
-op.add_argument("--disable-blink-features=AutomationControlled")
-#op.add_argument("--headless")
-op.add_experimental_option("useAutomationExtension",False)
-op.add_experimental_option("excludeSwitches", ["enable-automation"])
+import requests
+from bs4 import BeautifulSoup 
+import json
+headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36(KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36"
+}
+r = requests.get("https://tw.evga.com/products/productlist.aspx?type=0",headers=headers) #將此頁面的HTML GET下來
+soup = BeautifulSoup(r.text,"html.parser")
+sel = soup.select("div.grid-item")
 
-edge = webdriver.Edge("./msedgedriver.exe",options=op)
-edge.get('https://tw.evga.com/products/productlist.aspx?type=0')
-time.sleep(1)
-def take_gpu_info():
-    gpu_info_list = edge.find_elements(By.CLASS_NAME,"grid-item")
-    gpu_list = []
-    for gpus in gpu_info_list:
-        try:
-            gpu_str = gpus.find_element(By.CLASS_NAME,"btnAddCart").accessible_name.split(', ')
-            g_name = gpu_str[0].replace("Add ","")
-            g_pn = gpu_str[1] .replace(" ","")
-            g_price = gpus.find_element(By.CLASS_NAME,"pl-grid-price")
-            gpu = GPU(g_name,g_pn,int(g_price.text.split('*')[0].replace('NT$','')
-                                                            .replace(',','')
-                                                            .replace('.00','')
-                                                            .replace('\n','')))
-            gpu_list.append(gpu)
-        except:
-            pass
-    gpu_list.sort(key=lambda x: x.price)
-    return gpu_list
+def take_gpus():
+    GPU_shop = []   
+    for s in sel:
+        if s.select("input.btnAddCart"):
+            t = s.select_one("input.btnAddCart")
+            p = s.select_one("p.pl-grid-price strong")
+            t = t["title"].split(',')
+            gpu_name = t[0].replace('Add ','')
+            gpu_pn = t[1].replace(' ','')
+            price = int(p.text.replace(',',''))
+            gpu = {
+                "name" : gpu_name,
+                "pn"   : gpu_pn,
+                "price": price
+            }
+            GPU_shop.append(gpu)
+    GPU_shop.sort(key=lambda x: x["price"])
+    return GPU_shop
+def check(gpus,gpus_old):
+    on = []
+    down = []
 
+    for gpu in gpus:
+        e = 0
+        for gpu_old in gpus_old:
+            if gpu["pn"] == gpu_old["pn"]:
+                e = 1
+                break
+        if not e:
+            on.append(gpu)
+    for gpu_old in gpus_old:
+        e = 0
+        for gpu in gpus:
+            if gpu_old["pn"] == gpu["pn"]:
+                e = 1
+                break
+        if not e:
+            down.append(gpu_old)
+    if len(on):
+        for g in on:
+            print("上架了!!!")
+            print(g["name"])
+            print(g["price"])
+        return True
+    elif len(down):
+        for g in down:
+            print("下架了QQ")
+            print(g["name"])
+            print(g["price"])
+        return True
+    else:
+        return False #沒變 回傳false
+gpus = take_gpus()
 
-gpu_list_global = []
+with open("./gpu_shop.json","r") as f:
+    gpus_old = json.load(f)
+with open("./gpu_shop.json","w") as f:
+    json.dump(gpus,f)
+check(gpus,gpus_old)
 
-gpu_list_global = take_gpu_info()
-edge.quit()
-for gpu in gpu_list_global:
-    print(gpu.name)
