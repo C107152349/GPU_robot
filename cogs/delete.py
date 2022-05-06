ef8deb80d8ceb2d9ca01b8ff5dbd6293f0fee8ec
LAPTOP-PGEJU3SH\zxcas <C107152349@nkust.edu.tw>
2022-05-07 04:55:39 +0800

    new delete command

---
+++ cogs/delete.py
@@ -0,0 +1,13 @@
+from discord.ext import commands
+import discord
+from discord.ext.commands import bot
+from core.any import Cog_Extension
+import json
+
+class Delete(Cog_Extension):
+    @commands.command()
+    @commands.is_owner()
+    async def delete(self,ctx,limit=1):
+        await ctx.channel.purge(limit=limit)
+def setup(bot):
+    bot.add_cog(Delete(bot))
