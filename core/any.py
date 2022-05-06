ed92604276c68c393fafca4532ff733ea2492a03
C107152349 <C107152349@nkust.edu.tw>
2022-05-06 15:24:45 +0800

    s

--- core/any.py
+++ core/any.py
@@ -5,4 +5,3 @@ from discord.ext import commands
 class Cog_Extension(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-s = {"token" : "OTcxMTIyODU4NDU1NjAxMjEz.YnF6pA.sHr1NYCYa-d3wjwYuoc_SbmmwU4"}
