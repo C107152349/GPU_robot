c589eb5231a533afcd57ce47addcf3166149e187
C107152349 <C107152349@nkust.edu.tw>
2022-06-14 17:41:42 +0800

    delete useless function

--- core/any.py
+++ core/any.py
@@ -5,29 +5,7 @@ import json
 class Cog_Extension(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-def encode(string):
-    '''
-    將輸入的字串進行編碼
-    '''
-    e = ""
-    for s in string:
-        e = e + (chr(ord(s) + 1))
-    return e
-def decode(string):
-    '''
-    將輸入的字串進行解碼
-    '''
-    d = ""
-    for s in string:
-        d = d + (chr(ord(s) - 1))
-    return d
-def get_api(d:str):
-    """
-    輸入字串回傳該api
-    """
-    with open('./jsonstorage.json',"r",encoding="utf8") as file:
-        data = json.load(file)
-    return data[d]
+
 def update_proxy_ips(ip_count):
   start_time = time.time()
   response = requests.get("https://www.sslproxies.org/")
